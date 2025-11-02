a1b8784624b161ebf5d265ca213acce1ffaac7ec
mtouma56 <mtouma56@gmail.com>
2025-11-02 10:02:13 +0200

    Add bundle identifier test for Runner

--- ios/RunnerTests/RunnerTests.swift
+++ ios/RunnerTests/RunnerTests.swift
@@ -4,9 +4,14 @@ import XCTest
 
 class RunnerTests: XCTestCase {
 
-  func testExample() {
-    // If you add code to the Runner application, consider adding tests here.
-    // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+  func testBundleIdentifierMatchesExpectedValue() {
+    let bundleIdentifier = Bundle.main.bundleIdentifier
+
+    XCTAssertEqual(
+      bundleIdentifier,
+      "com.cocody_market_manager.app.testProject",
+      "Runner bundle identifier should remain stable"
+    )
   }
 
 }
